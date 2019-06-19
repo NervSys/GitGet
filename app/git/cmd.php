b6dfb4443f672f3f4e30d3b82443752c2a7eda27
Jerry <27206617@qq.com>
2019-06-19 17:04:18 +0800

    rebuild git control commands

--- app/git/cmd.php
+++ app/git/cmd.php
@@ -9,11 +9,17 @@ class cmd extends factory
 {
     private $git_cmd = [];
 
+    /**
+     * cmd constructor.
+     *
+     * @param string $git_url
+     * @param string $local_path
+     */
     public function __construct(string $git_url, string $local_path)
     {
         conf::load('app/git', 'git_cmd');
-        
-        $this->git_cmd = conf::get('cmd');
+
+        $this->git_cmd = conf::get('git_cmd');
 
         if (!is_dir($local_path)) {
             mkdir($local_path, 0777, true);
@@ -25,7 +31,11 @@ class cmd extends factory
         chdir($local_path);
     }
 
-
+    /**
+     * @param string $branch
+     *
+     * @return array
+     */
     public function pull(string $branch): array
     {
         exec($this->build_cmd($this->git_cmd['pull'], $branch), $output);
@@ -33,7 +43,11 @@ class cmd extends factory
         return $output;
     }
 
-
+    /**
+     * @param string $commit
+     *
+     * @return array
+     */
     public function reset(string $commit): array
     {
         exec($this->build_cmd($this->git_cmd['reset'], $commit), $output);
@@ -41,6 +55,9 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @return array
+     */
     public function clean(): array
     {
         exec($this->build_cmd($this->git_cmd['clean']), $output);
@@ -48,6 +65,11 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @param string $branch
+     *
+     * @return array
+     */
     public function delete(string $branch): array
     {
         exec($this->build_cmd($this->git_cmd['delete'], $branch), $output);
@@ -55,7 +77,11 @@ class cmd extends factory
         return $output;
     }
 
-
+    /**
+     * @param string $branch
+     *
+     * @return array
+     */
     public function checkout(string $branch): array
     {
         exec($this->build_cmd($this->git_cmd['checkout'], $branch), $output);
@@ -63,6 +89,9 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @return array
+     */
     public function stash_save(): array
     {
         exec($this->build_cmd($this->git_cmd['stash_save']), $output);
@@ -70,6 +99,9 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @return array
+     */
     public function stash_apply(): array
     {
         exec($this->build_cmd($this->git_cmd['stash_apply']), $output);
@@ -77,6 +109,9 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @return array
+     */
     public function local_branch(): array
     {
         exec($this->build_cmd($this->git_cmd['local_branch']), $output);
@@ -84,6 +119,9 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @return array
+     */
     public function remote_branch(): array
     {
         exec($this->build_cmd($this->git_cmd['remote_branch']), $output);
@@ -91,6 +129,12 @@ class cmd extends factory
         return $output;
     }
 
+    /**
+     * @param string $name
+     * @param string $email
+     *
+     * @return array
+     */
     public function set_config(string $name, string $email): array
     {
         exec($this->build_cmd($this->git_cmd['set_name'], $name), $output);
@@ -99,7 +143,12 @@ class cmd extends factory
         return $output;
     }
 
-
+    /**
+     * @param string $cmd
+     * @param string ...$params
+     *
+     * @return string
+     */
     private function build_cmd(string $cmd, string ...$params): string
     {
         return escapeshellcmd(sprintf($cmd, ...$params));
