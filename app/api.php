be807ce58349ced9d91ec07eec564bc26fd4bf78
liu <2579186091@qq.com>
2019-07-03 16:38:51 +0800

    gitmodules

--- app/api.php
+++ app/api.php
@@ -21,8 +21,14 @@
 //Declare strict types
 declare(strict_types = 1);
 
+//Define root path
+define('ROOT', __DIR__ . '/../');
+
+//Define entry path
+define('ENTRY_SCRIPT', __FILE__);
+
 //Load system script
-require __DIR__ . '/../core/system.php';
+require __DIR__ . '/../NervSys/core/system.php';
 
 //Boot system
 \core\system::boot();
