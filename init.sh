07ca4251e41c50849bb081ccd6182517808ca910
liu <2579186091@qq.com>
2019-08-24 15:57:18 +0800

    初始化

--- init.sh
+++ init.sh
@@ -17,14 +17,16 @@ done
 
 #添加组和用户
 sed -i '/'"2000"'/d' /etc/group
+sed -i '/'"www"'/d' /etc/group
 echo $user:x:2000: >> /etc/group
 sed -i '/'"2000"'/d' /etc/passwd
+sed -i '/'"www"'/d' /etc/passwd
 echo $user:x:2000:2000:root:$local_path:/bin/bash >> /etc/passwd
 
 #设置ssh配置
 sed -i '/'"StrictHostKeyChecking no"'/d' /etc/ssh/ssh_config
 echo StrictHostKeyChecking no >> /etc/ssh/ssh_config
-sed -i '/'"UserKnownHostsFile /dev/null"'/d' /etc/ssh/ssh_config
+sed -i '/'"UserKnownHostsFile"'/d' /etc/ssh/ssh_config
 echo UserKnownHostsFile /dev/null >> /etc/ssh/ssh_config
 
 #权限处理
@@ -33,6 +35,6 @@ mkdir $local_path/.ssh -p
 fi
 
 chmod 777 -R "$local_path"
-chown $user:$user -R "$local_path"
+chown $user:$user -R "$local_path/.."
 
 php html/api.php -c"project/proj_git-set_home_path" -d"home_path=$local_path"
